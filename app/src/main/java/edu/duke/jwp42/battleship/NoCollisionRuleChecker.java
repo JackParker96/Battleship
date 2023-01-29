c8e67435b47dd0d0bd24e2b562a0c907fec9d35f
Jack Parker <jack.parker@duke.edu>
2023-01-29 16:31:49 -0500

    Completed Task 14

--- app/src/main/java/edu/duke/jwp42/battleship/NoCollisionRuleChecker.java
+++ app/src/main/java/edu/duke/jwp42/battleship/NoCollisionRuleChecker.java
@@ -1,11 +1,16 @@
 package edu.duke.jwp42.battleship;
 
+/**
+ * Used to check if a particular placement for a particular ship on a particular
+ * board would result in any collisions with other ships
+ */
 public class NoCollisionRuleChecker<T> extends PlacementRuleChecker<T> {
 
   @Override
   protected boolean checkMyRule(Ship<T> theShip, Board<T> theBoard) {
-    // use .getCoordinate() method of Ship to get iterable of Coordinates 
-    //use .whatIsAt(Coordinate c) method of Board to make sure all squares needed by theShip are null
+    // use .getCoordinate() method of Ship to get iterable of Coordinates
+    // use .whatIsAt(Coordinate c) method of Board to make sure all squares needed
+    // by theShip are null
     Iterable<Coordinate> coords = theShip.getCoordinates();
     for (Coordinate c : coords) {
       if (theBoard.whatIsAt(c) != null) {
@@ -18,5 +23,5 @@ public class NoCollisionRuleChecker<T> extends PlacementRuleChecker<T> {
   public NoCollisionRuleChecker(PlacementRuleChecker<T> next) {
     super(next);
   }
-  
+
 }
